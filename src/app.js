84f298db11b4071dffc3582f5ba9a1e1e45c6963
cyveros <cyveros@gmail.com>
2017-01-05 00:55:26 -0500

    remove debug code

--- src/app.js
+++ src/app.js
@@ -35,8 +35,6 @@ app.use('/', RouterProvider);
 
 mongoose.Promise = global.Promise;
 
-console.log(config.db);
-
 mongoose.connect(config.db, { server: { socketOptions: { keepAlive: 1 } } });
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${config.db}`);
